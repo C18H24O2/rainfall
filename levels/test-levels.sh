eb22e799da2ccf9cc165eeb4218bdc588fed8783
xtrm <oss@xtrm.me>
2025-09-09 17:28:42 +0200

    🐛 fix(test-levels.sh): disable ssh key authenticity
    Signed-off-by: xtrm <oss@xtrm.me>

--- levels/test-levels.sh
+++ levels/test-levels.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -o pipefail
+
 if [ $# -ne 1 ]; then
 	echo "Usage: $0 <ip>"
 	exit 1
@@ -20,13 +22,21 @@ function test() {
 	echo -n "Testing $lvl flag..."
 	local flag=$(cat $lvl/flag)
 	local next=$2
-	SSHPASS="$flag" sshpass -e ssh $next@$IP -p 4242 "exit" >/dev/null 2>&1
+	SSHPASS="$flag" sshpass -e ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $next@$IP -p 4242 "exit" >/dev/null 2>&1
 	if [ $? -ne 0 ]; then
 		echo " KO"
 		return
 	fi
 	echo " OK"
 }
+
+echo "Doing a test connect on level0..."
+SSHPASS="level0" sshpass -e ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no level0@$IP -p 4242 "exit"
+if [ $? -ne 0 ]; then
+	echo "Could not connect to level0, something's wrong"
+#	echo "Maybe try running 'ssh -p 4242 level0@$IP' first to accept "
+	exit 1
+fi
 
 for lvl in {0..8}; do
 	test "level$lvl" "level$((lvl+1))"
