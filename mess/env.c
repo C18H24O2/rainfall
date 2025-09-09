1753dea037e642f95b82ce0d8f1060ef13870fe3
xtrm <oss@xtrm.me>
2025-09-09 19:19:57 +0200

    🔨 chore: cleanup the mess/ folder
    Signed-off-by: xtrm <oss@xtrm.me>

--- mess/env.c
+++
@@ -1,1 +0,0 @@
-int main(int argc,char**argv,char**envp){int i;for(i=0;envp[i]!=0;i++)printf("%p => %s\n",envp[i],envp[i]);}
--- mess/stringgen.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-if len(sys.argv) == 1:
-    print("".join(["a" + chr(i) for i in range(65, 91)]))
-    print("".join(["b" + chr(i) for i in range(65, 91)]))
-else:
-    print("A" * int(sys.argv[1]))
-    print("B" * int(sys.argv[1]))
